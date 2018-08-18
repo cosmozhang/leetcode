2022f7b2bb80e006b1f054d04e1b1a6207055abd
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-08-18 11:30:04 -0400

    update 19.remove-nth-node-from-end-of-list.py

---
+++ 19.remove-nth-node-from-end-of-list.py
@@ -0,0 +1,59 @@
+#
+# [19] Remove Nth Node From End of List
+#
+# https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+#
+# algorithms
+# Medium (33.77%)
+# Total Accepted:    278.1K
+# Total Submissions: 824.1K
+# Testcase Example:  '[1,2,3,4,5]\n2'
+#
+# Given a linked list, remove the n-th node from the end of list and return its
+# head.
+# 
+# Example:
+# 
+# 
+# Given linked list: 1->2->3->4->5, and n = 2.
+# 
+# After removing the second node from the end, the linked list becomes
+# 1->2->3->5.
+# 
+# 
+# Note:
+# 
+# Given n will always be valid.
+# 
+# Follow up:
+# 
+# Could you do this in one pass?
+# 
+#
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        
+        l = head
+        r = head
+        for i in range(n):
+            r = r.next
+        if r == None:
+            return head.next
+        else:
+            while r.next:
+                r = r.next
+                l = l.next
+            l.next = l.next.next
+        
+            return head
