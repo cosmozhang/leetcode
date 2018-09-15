ed4980c40cb931abe2b6a8de2707f8e5346461fd
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-09-15 08:27:42 -0400

    update 71.simplify-path.py

---
+++ 71.simplify-path.py
@@ -0,0 +1,63 @@
+#
+# [71] Simplify Path
+#
+# https://leetcode.com/problems/simplify-path/description/
+#
+# algorithms
+# Medium (26.82%)
+# Total Accepted:    124.2K
+# Total Submissions: 460.3K
+# Testcase Example:  '"/home/"'
+#
+# Given an absolute path for a file (Unix-style), simplify it. 
+# 
+# For example,
+# path = "/home/", => "/home"
+# path = "/a/./b/../../c/", => "/c"
+# path = "/a/../../b/../c//.//", => "/c"
+# path = "/a//b////c/d//././/..", => "/a/b/c"
+# 
+# In a UNIX-style file system, a period ('.') refers to the current directory,
+# so it can be ignored in a simplified path. Additionally, a double period
+# ("..") moves up a directory, so it cancels out whatever the last directory
+# was. For more information, look here:
+# https://en.wikipedia.org/wiki/Path_(computing)#Unix_style
+# 
+# Corner Cases:
+# 
+# 
+# Did you consider the case where path = "/../"?
+# In this case, you should return "/".
+# Another corner case is the path might contain multiple slashes '/' together,
+# such as "/home//foo/".
+# In this case, you should ignore redundant slashes and return "/home/foo".
+# 
+# 
+#
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+
+        if not path or len(path) == 1:
+            return path
+
+        stack = ['/']
+        path_ls = path.split('/')
+        for item in path_ls:
+            if item == '..':
+                if not stack:
+                    stack.append('/')
+                elif len(stack) == 1 and stack[-1] == '/':
+                    continue
+                else:
+                    stack.pop()
+            elif item == '.' or item == '':
+                continue
+            else:
+                stack.append(item)
+
+        return stack[0] + '/'.join(stack[1:])
+            
