0c314c0e4be49ca82c7b372a66df0963d1bf46ac
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-10-18 08:41:25 -0400

    update 107.binary-tree-level-order-traversal-ii.py

---
+++ 107.binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,66 @@
+#
+# [107] Binary Tree Level Order Traversal II
+#
+# https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+#
+# algorithms
+# Easy (43.97%)
+# Total Accepted:    187.9K
+# Total Submissions: 426.8K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# Given a binary tree, return the bottom-up level order traversal of its nodes'
+# values. (ie, from left to right, level by level from leaf to root).
+# 
+# 
+# For example:
+# Given binary tree [3,9,20,null,null,15,7],
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+# 
+# 
+# return its bottom-up level order traversal as:
+# 
+# [
+# ⁠ [15,7],
+# ⁠ [9,20],
+# ⁠ [3]
+# ]
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        
+        ret_ls = []
+        s = [root]
+        while s:
+            new_s = []
+            level_vs = []
+            for node in s:
+                level_vs+=[node.val]
+                if node.left:
+                    new_s += [node.left]
+                if node.right:
+                    new_s += [node.right]
+            s = new_s
+            ret_ls += [level_vs]
+        return ret_ls[::-1]
