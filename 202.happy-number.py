d583ebc5d852ddd74318d656011c36edc79ebbaf
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-10-25 01:40:31 -0400

    update 202.happy-number.py

---
+++ 202.happy-number.py
@@ -0,0 +1,50 @@
+#
+# [202] Happy Number
+#
+# https://leetcode.com/problems/happy-number/description/
+#
+# algorithms
+# Easy (42.94%)
+# Total Accepted:    189.4K
+# Total Submissions: 440.6K
+# Testcase Example:  '19'
+#
+# Write an algorithm to determine if a number is "happy".
+# 
+# A happy number is a number defined by the following process: Starting with
+# any positive integer, replace the number by the sum of the squares of its
+# digits, and repeat the process until the number equals 1 (where it will
+# stay), or it loops endlessly in a cycle which does not include 1. Those
+# numbers for which this process ends in 1 are happy numbers.
+# 
+# Example: 
+# 
+# 
+# Input: 19
+# Output: true
+# Explanation: 
+# 12 + 92 = 82
+# 82 + 22 = 68
+# 62 + 82 = 100
+# 12 + 02 + 02 = 1
+# 
+#
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if not n:
+            return False
+        hist_set=set()
+
+        while True:
+            if n == 1:
+                return True
+            else:
+                if n in hist_set:
+                    return False
+
+            hist_set.add(n)
+            n = sum([int(i)**2 for i in str(n)])
