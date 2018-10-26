4e0e31b576c7591855ede647facd05c7282351fe
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-10-25 23:19:58 -0400

    update 215.kth-largest-element-in-an-array.py

---
+++ 215.kth-largest-element-in-an-array.py
@@ -0,0 +1,44 @@
+#
+# [215] Kth Largest Element in an Array
+#
+# https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+#
+# algorithms
+# Medium (43.19%)
+# Total Accepted:    269.1K
+# Total Submissions: 619.8K
+# Testcase Example:  '[3,2,1,5,6,4]\n2'
+#
+# Find the kth largest element in an unsorted array. Note that it is the kth
+# largest element in the sorted order, not the kth distinct element.
+# 
+# Example 1:
+# 
+# 
+# Input: [3,2,1,5,6,4] and k = 2
+# Output: 5
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: [3,2,3,1,2,4,5,5,6] and k = 4
+# Output: 4
+# 
+# Note: 
+# You may assume k is always valid, 1 ≤ k ≤ array's length.
+# 
+#
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+        sorted_nums = sorted(nums, reverse=True)
+        
+        return sorted_nums[k-1]
+
+        
