31be271a47bf580c676eaadd3477c992f199c51a
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-10-29 23:48:23 -0400

    update 277.find-the-celebrity.py

--- 277.find-the-celebrity.py
+++ 277.find-the-celebrity.py
@@ -49,7 +49,6 @@ class Solution(object):
         :rtype: int
         """
         
-
         candidate = 0
 
         for i in range(1, n):
@@ -57,8 +56,7 @@ class Solution(object):
                 candidate = i
 
         for i in range(n):
-            if i != candidate:
-                if (knows(candidate, i) or not knows(i, candidate)):
-                    return -1
+            if i != candidate and (knows(candidate, i) or not knows(i, candidate)):
+                return -1
 
         return candidate
