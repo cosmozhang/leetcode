f04cb6685d1c09cf4942e0022a60ff52d270142a
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-11-07 22:26:02 -0500

    update 280.wiggle-sort.py

---
+++ 280.wiggle-sort.py
@@ -0,0 +1,44 @@
+#
+# [280] Wiggle Sort
+#
+# https://leetcode.com/problems/wiggle-sort/description/
+#
+# algorithms
+# Medium (59.67%)
+# Total Accepted:    54.6K
+# Total Submissions: 91.4K
+# Testcase Example:  '[3,5,2,1,6,4]'
+#
+# Given an unsorted array nums, reorder it in-place such that nums[0] <=
+# nums[1] >= nums[2] <= nums[3]....
+# 
+# Example:
+# 
+# 
+# Input: nums = [3,5,2,1,6,4]
+# Output: One possible answer is [3,5,1,6,2,4]
+# 
+#
+class Solution(object):
+    def wiggleSort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+                
+        nums.sort()
+        n = len(nums)
+
+        if n%2==0:
+            #even
+            v = n/2
+        else:
+            #odd
+            v = n/2+1
+        for s, e in zip(range(1, n, 2), range(v, n)):
+            t = nums[e]
+            nums[s+1:e+1] = nums[s:e]
+            nums[s] = t
+
+                
+        
