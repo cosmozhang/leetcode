57c45b111e380811ac569d3ccea1820bfe5dbc8f
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-11-10 19:26:47 -0500

    update 653.two-sum-iv-input-is-a-bst.py

---
+++ 653.two-sum-iv-input-is-a-bst.py
@@ -0,0 +1,82 @@
+#
+# [653] Two Sum IV - Input is a BST
+#
+# https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
+#
+# algorithms
+# Easy (51.07%)
+# Total Accepted:    62.5K
+# Total Submissions: 122.3K
+# Testcase Example:  '[5,3,6,2,4,null,7]\n9'
+#
+# Given a Binary Search Tree and a target number, return true if there exist
+# two elements in the BST such that their sum is equal to the given target.
+# 
+# Example 1:
+# 
+# Input: 
+# ⁠   5
+# ⁠  / \
+# ⁠ 3   6
+# ⁠/ \   \
+# 2   4   7
+# 
+# Target = 9
+# 
+# Output: True
+# 
+# 
+# 
+# 
+# Example 2:
+# 
+# Input: 
+# ⁠   5
+# ⁠  / \
+# ⁠ 3   6
+# ⁠/ \   \
+# 2   4   7
+# 
+# Target = 28
+# 
+# Output: False
+# 
+# 
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        
+        to_minus = []
+        q = [root]
+
+        while q:
+            nq = []
+            for n in q:
+                to_minus += [n.val]
+                if n.left:
+                    nq += [n.left]
+                if n.right:
+                    nq += [n.right]
+            q = nq
+
+        for i, v in enumerate(to_minus):
+            if k-v in to_minus[:i]+to_minus[i+1:]:
+                return True
+        return False
+
