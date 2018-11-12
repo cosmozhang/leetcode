8e78c769ff05638f7dba48912858b4fec12d4f03
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-11-11 23:06:45 -0500

    update 346.moving-average-from-data-stream.py

---
+++ 346.moving-average-from-data-stream.py
@@ -0,0 +1,53 @@
+#
+# [346] Moving Average from Data Stream
+#
+# https://leetcode.com/problems/moving-average-from-data-stream/description/
+#
+# algorithms
+# Easy (62.98%)
+# Total Accepted:    57K
+# Total Submissions: 90.4K
+# Testcase Example:  '["MovingAverage","next","next","next","next"]\n[[3],[1],[10],[3],[5]]'
+#
+# Given a stream of integers and a window size, calculate the moving average of
+# all integers in the sliding window.
+# 
+# Example:
+# 
+# 
+# MovingAverage m = new MovingAverage(3);
+# m.next(1) = 1
+# m.next(10) = (1 + 10) / 2
+# m.next(3) = (1 + 10 + 3) / 3
+# m.next(5) = (10 + 3 + 5) / 3
+# 
+# 
+# 
+# 
+#
+class MovingAverage(object):
+
+    def __init__(self, size):
+        """
+        Initialize your data structure here.
+        :type size: int
+        """
+        self.size = size
+        self.q = []
+
+    def next(self, val):
+        """
+        :type val: int
+        :rtype: float
+        """
+        self.q.append(val)
+        if len(self.q) > self.size:
+            self.q.pop(0)
+
+        return sum(self.q)*1.0/len(self.q)
+        
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
