76dcb7d09c82ccfec78a23a9c10a2eb6d92dcf35
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-11-11 22:53:02 -0500

    update 852.peak-index-in-a-mountain-array.py

---
+++ 852.peak-index-in-a-mountain-array.py
@@ -0,0 +1,64 @@
+#
+# [882] Peak Index in a Mountain Array
+#
+# https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+#
+# algorithms
+# Easy (67.22%)
+# Total Accepted:    38.1K
+# Total Submissions: 56.7K
+# Testcase Example:  '[0,1,0]'
+#
+# Let's call an array A a mountain if the following properties hold:
+# 
+# 
+# A.length >= 3
+# There exists some 0 < i < A.length - 1 such that A[0] < A[1] < ... A[i-1] <
+# A[i] > A[i+1] > ... > A[A.length - 1]
+# 
+# 
+# Given an array that is definitely a mountain, return any i such that A[0] <
+# A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1].
+# 
+# Example 1:
+# 
+# 
+# Input: [0,1,0]
+# Output: 1
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: [0,2,1,0]
+# Output: 1
+# 
+# 
+# Note:
+# 
+# 
+# 3 <= A.length <= 10000
+# 0 <= A[i] <= 10^6
+# A is a mountain, as defined above.
+# 
+# 
+#
+class Solution(object):
+    def peakIndexInMountainArray(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        
+        l = 0
+        r= len(A) -1
+
+        while l< r:
+            m = (l+r)/2
+            if A[m] < A[m-1]:
+                r = m
+            elif A[m] < A[m+1]:
+                l = m
+            else:
+                return m
