21ac7062e2fda1aa93c620c23b6c4ae89bb3f1b8
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-11-22 23:35:49 -0500

    update 678.valid-parenthesis-string.py

---
+++ 678.valid-parenthesis-string.py
@@ -0,0 +1,89 @@
+#
+# [678] Valid Parenthesis String
+#
+# https://leetcode.com/problems/valid-parenthesis-string/description/
+#
+# algorithms
+# Medium (30.70%)
+# Total Accepted:    18.7K
+# Total Submissions: 60.7K
+# Testcase Example:  '"()"'
+#
+# 
+# Given a string containing only three types of characters: '(', ')' and '*',
+# write a function to check whether this string is valid. We define the
+# validity of a string by these rules:
+# 
+# Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+# Any right parenthesis ')' must have a corresponding left parenthesis '('.
+# Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+# '*' could be treated as a single right parenthesis ')' or a single left
+# parenthesis '(' or an empty string.
+# An empty string is also valid.
+# 
+# 
+# 
+# Example 1:
+# 
+# Input: "()"
+# Output: True
+# 
+# 
+# 
+# Example 2:
+# 
+# Input: "(*)"
+# Output: True
+# 
+# 
+# 
+# Example 3:
+# 
+# Input: "(*))"
+# Output: True
+# 
+# 
+# 
+# Note:
+# 
+# The string size will be in the range [1, 100].
+# 
+# We need two stacks to record the idx, one for '(' n the other for '*'.
+#
+class Solution(object):
+    def checkValidString(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        if len(s) == 0:
+            return True
+
+        pstack = []
+        sstack = []
+
+        for idx, t in enumerate(s):
+            if t == '(':
+                pstack.append(idx)
+            elif t == '*':
+                sstack.append(idx)
+            else:
+                if len(pstack) == 0 and len(sstack) == 0:
+                    return False
+                elif len(pstack) == 0:
+                    sstack.pop(-1)
+                else:
+                    pstack.pop(-1)
+
+        while len(pstack) > 0:
+            if len(sstack) == 0:
+                return False
+            elif sstack[-1] > pstack[-1]:
+                sstack.pop(-1)
+                pstack.pop(-1)
+            else:
+                return False
+        return True
+                    
+                
