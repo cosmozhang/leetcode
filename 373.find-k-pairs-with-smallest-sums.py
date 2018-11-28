9772f01f414ad86baf2e069fb313ab36ad95dbc0
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-11-28 15:10:42 -0500

    update 373.find-k-pairs-with-smallest-sums.py

--- 373.find-k-pairs-with-smallest-sums.py
+++ 373.find-k-pairs-with-smallest-sums.py
@@ -41,26 +41,8 @@
 # Explanation: All possible pairs are returned from the sequence: [1,3],[2,3]
 # 
 #
+# use heap
 class Solution(object):
-    def kSmallestPairs(self, nums1, nums2, k):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :type k: int
-        :rtype: List[List[int]]
-        """
-        '''
-        res_dic = {}
-        
-        for idx1, n1 in enumerate(nums1[:k]):
-            for idx2, n2 in enumerate(nums2[:k]):
-                res_dic[(n1+n2, idx1, idx2)] = [n1, n2]
-
-
-        res = sorted(res_dic.iteritems(), key=lambda x: x[0][0])
-
-        return [list(r[1]) for r in res[:k]]
-        '''
 
     def kSmallestPairs(self, nums1, nums2, k):
         """
@@ -72,7 +54,7 @@ class Solution(object):
         ans = []
         heap = [(float('inf'), None, None)]
 
-        heapq.heapify(heap)
+        # heapq.heapify(heap)
         
         m, n = len(nums1), len(nums2)
         ret_ls = []
