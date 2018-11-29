0875b3f45841d03c811ca390f14e7e062c5107eb
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-11-29 14:49:53 -0500

    update 349.intersection-of-two-arrays.py

--- 349.intersection-of-two-arrays.py
+++ 349.intersection-of-two-arrays.py
@@ -10,31 +10,31 @@
 # Testcase Example:  '[1,2,2,1]\n[2,2]'
 #
 # Given two arrays, write a function to compute their intersection.
-# 
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: nums1 = [1,2,2,1], nums2 = [2,2]
 # Output: [2]
-# 
-# 
-# 
+#
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 # Output: [9,4]
-# 
-# 
+#
+#
 # Note:
-# 
-# 
+#
+#
 # Each element in the result must be unique.
 # The result can be in any order.
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 #
 class Solution(object):
     def intersection(self, nums1, nums2):
@@ -43,7 +43,5 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        a = set(nums1)
-        a.intersection_update(set(nums2))
 
-        return list(a)
+        return list(set(nums1) & set(nums2))
