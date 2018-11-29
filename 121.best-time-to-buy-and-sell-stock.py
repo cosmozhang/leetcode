4c2c411cef575ce9dfb02a08d3cd2698c4df602a
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-11-29 16:02:53 -0500

    update 121.best-time-to-buy-and-sell-stock.py

--- 121.best-time-to-buy-and-sell-stock.py
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -11,31 +11,31 @@
 #
 # Say you have an array for which the ith element is the price of a given stock
 # on day i.
-# 
+#
 # If you were only permitted to complete at most one transaction (i.e., buy one
 # and sell one share of the stock), design an algorithm to find the maximum
 # profit.
-# 
+#
 # Note that you cannot sell a stock before you buy one.
-# 
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: [7,1,5,3,6,4]
 # Output: 5
 # Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit =
 # 6-1 = 5.
 # Not 7-1 = 6, as selling price needs to be larger than buying price.
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: [7,6,4,3,1]
 # Output: 0
 # Explanation: In this case, no transaction is done, i.e. max profit = 0.
-# 
-# 
+#
+#
 #
 class Solution(object):
     def maxProfit(self, prices):
@@ -45,15 +45,15 @@ class Solution(object):
         """
         if not prices:
             return 0
-        
-        dp = [0 for i in range(len(prices))]
+
+        # dp = [0 for i in range(len(prices))]
         min_v = prices[0]
         max_p = 0
 
         for i in range(1, len(prices)):
             min_v = min(prices[i], min_v)
-            dp[i] = max(dp[i-1], prices[i]-min_v)
-            max_p = max(dp[i], max_p)
+            # dp[i] = max(dp[i-1], prices[i]-min_v)
+            max_p = max(prices[i]-min_v, max_p)
 
         return max_p
-            
+
