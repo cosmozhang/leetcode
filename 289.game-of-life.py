f753e5f42191bf651ae4c1d86912648f7a0884d5
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-11-29 10:48:46 -0500

    update 289.game-of-life.py

--- 289.game-of-life.py
+++ 289.game-of-life.py
@@ -12,13 +12,13 @@
 # According to the Wikipedia's article: "The Game of Life, also known simply as
 # Life, is a cellular automaton devised by the British mathematician John
 # Horton Conway in 1970."
-# 
+#
 # Given a board with m by n cells, each cell has an initial state live (1) or
 # dead (0). Each cell interacts with its eight neighbors (horizontal, vertical,
 # diagonal) using the following four rules (taken from the above Wikipedia
 # article):
-# 
-# 
+#
+#
 # Any live cell with fewer than two live neighbors dies, as if caused by
 # under-population.
 # Any live cell with two or three live neighbors lives on to the next
@@ -27,43 +27,43 @@
 # over-population..
 # Any dead cell with exactly three live neighbors becomes a live cell, as if by
 # reproduction.
-# 
-# 
+#
+#
 # Write a function to compute the next state (after one update) of the board
 # given its current state. The next state is created by applying the above
 # rules simultaneously to every cell in the current state, where births and
 # deaths occur simultaneously.
-# 
+#
 # Example:
-# 
-# 
-# Input: 
+#
+#
+# Input:
 # [
 # [0,1,0],
 # [0,0,1],
 # [1,1,1],
 # [0,0,0]
 # ]
-# Output: 
+# Output:
 # [
 # [0,0,0],
 # [1,0,1],
 # [0,1,1],
 # [0,1,0]
 # ]
-# 
-# 
+#
+#
 # Follow up:
-# 
-# 
+#
+#
 # Could you solve it in-place? Remember that the board needs to be updated at
 # the same time: You cannot update some cells first and then use their updated
 # values to update other cells.
 # In this question, we represent the board using a 2D array. In principle, the
 # board is infinite, which would cause problems when the active area encroaches
 # the border of the array. How would you address these problems?
-# 
-# 
+#
+#
 #
 class Solution(object):
     def gameOfLife(self, board):
@@ -72,50 +72,47 @@ class Solution(object):
         :rtype: void Do not return anything, modify board in-place instead.
         """
         m, n = len(board), len(board[0])
-        
-        record_board = [[False for _ in range(n)] for _ in range(m)]
 
         for i in range(m):
             for j in range(n):
-                record_board[i][j] = self.helper(i, j, board, m, n)
+                board[i][j] = self.helper(i, j, board, m, n)
 
         for i in range(m):
             for j in range(n):
-                if record_board[i][j]:
-                    board[i][j] = 1 - board[i][j]
-
-                
+                if board[i][j] >= 2:
+                    board[i][j] = 3 - board[i][j]
 
     def helper(self, i, j, board, m, n):
         s = 0
 
         if i-1 >= 0:
-            s += board[i-1][j]
+            s += board[i-1][j] % 2
         if i-1 >= 0 and j-1>=0:
-            s += board[i-1][j-1]
+            s += board[i-1][j-1] % 2
         if i-1 >= 0 and j+1 < n:
-            s += board[i-1][j+1]
+            s += board[i-1][j+1] % 2
         if i+1 < m:
-            s += board[i+1][j]
+            s += board[i+1][j] % 2
         if i+1 < m and j-1 >= 0:
-            s += board[i+1][j-1]
+            s += board[i+1][j-1] % 2
         if i+1 < m and j+1 < n:
-            s += board[i+1][j+1]
+            s += board[i+1][j+1] % 2
         if j-1 >= 0:
-            s += board[i][j-1]
+            s += board[i][j-1] % 2
         if j+1 < n:
-            s += board[i][j+1]
+            s += board[i][j+1] % 2
 
         if board[i][j] == 1:
             if s < 2:
-                return True
-            elif 2<=s and s<=3:
-                return False
+                return 3
+            elif 2 <= s and s <= 3:
+                return 1
             elif 3 < s:
-                return True
+                return 3
         else:
             if s == 3:
-                return True
-        return False
-                
-        
+                return 2
+            else:
+                return 0
+
+
