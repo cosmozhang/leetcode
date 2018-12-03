b350ce1082daba11ea59ce885b8a353ddba21d58
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-12-03 18:39:27 -0500

    update 31.next-permutation.py

--- 31.next-permutation.py
+++ 31.next-permutation.py
@@ -11,19 +11,19 @@
 #
 # Implement next permutation, which rearranges numbers into the
 # lexicographically next greater permutation of numbers.
-# 
+#
 # If such arrangement is not possible, it must rearrange it as the lowest
 # possible order (ie, sorted in ascending order).
-# 
+#
 # The replacement must be in-place and use only constant extra memory.
-# 
+#
 # Here are some examples. Inputs are in the left-hand column and its
 # corresponding outputs are in the right-hand column.
-# 
+#
 # 1,2,3 → 1,3,2
 # 3,2,1 → 1,2,3
 # 1,1,5 → 1,5,1
-# 
+#
 #
 class Solution(object):
     def nextPermutation(self, nums):
@@ -32,6 +32,7 @@ class Solution(object):
         :rtype: void Do not return anything, modify nums in-place instead.
         """
 
+        '''
         if len(nums) == 1:
             nums = nums
         idx = len(nums) - 1
@@ -50,10 +51,26 @@ class Solution(object):
             else:
                 for j in range(idx, len(nums)):
                     if nums[j] <= nums[toswap_idx]:
-                        tobeswap_idx = j-1
+                        tobeswap_idx = j-1 # the 1st num > nums[toswap_idx]
                         break
-            temp = nums[toswap_idx]
-            nums[toswap_idx] = nums[tobeswap_idx]
-            nums[tobeswap_idx] = temp
+            nums[toswap_idx], nums[tobeswap_idx] = nums[tobeswap_idx], nums[toswap_idx]
             nums[idx:] = nums[idx:][::-1]
+        '''
 
+        
+        to_right_idx = -1
+        for i in range(len(nums) - 1, -1, -1):
+            if i-1 > -1 and nums[i-1] < nums[i]:
+                to_right_idx = i-1
+                break
+
+        if to_right_idx == -1:
+            nums[:] = nums[::-1]
+        else:
+            r = nums[to_right_idx]
+            for j in range(len(nums) - 1, to_right_idx, -1):
+                if nums[j] > r:
+                    to_left_idx = j
+                    break
+            nums[to_left_idx], nums[to_right_idx] = nums[to_right_idx], nums[to_left_idx]
+            nums[to_right_idx+1:] = nums[to_right_idx+1:][::-1]
