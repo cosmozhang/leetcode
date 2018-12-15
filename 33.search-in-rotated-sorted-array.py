32019505afb45f8ebf14c014d0317cbe1c278536
Xiao Zhang <cosmozhang@users.noreply.github.com>
2018-12-15 15:43:32 -0500

    update 33.search-in-rotated-sorted-array.py

--- 33.search-in-rotated-sorted-array.py
+++ 33.search-in-rotated-sorted-array.py
@@ -11,30 +11,30 @@
 #
 # Suppose an array sorted in ascending order is rotated at some pivot unknown
 # to you beforehand.
-# 
+#
 # (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
-# 
+#
 # You are given a target value to search. If found in the array return its
 # index, otherwise return -1.
-# 
+#
 # You may assume no duplicate exists in the array.
-# 
+#
 # Your algorithm's runtime complexity must be in the order of O(log n).
-# 
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: nums = [4,5,6,7,0,1,2], target = 0
 # Output: 4
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: nums = [4,5,6,7,0,1,2], target = 3
 # Output: -1
-# 
 #
+# First determine which side the middle point is located, then determine which side the target is located.
 class Solution(object):
     def search(self, nums, target):
         """
@@ -42,11 +42,11 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        '''
         l = 0
         r = len(nums) - 1
         ret = -1
         while l <= r:
-            print l, r
             m = (l+r)/2
             if nums[m] == target:
                 ret = m
@@ -62,5 +62,24 @@ class Solution(object):
                 else:
                     r = m-1
         return ret
+        '''
 
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            m = (l+r) >> 1
+            if nums[m] == target:
+                return m
+            if nums[m] >= nums[l]:
+                if target >= nums[l] and target < nums[m]:
+                    r = m-1
+                else:
+                    l = m+1
+            elif nums[m] < nums[l]:
+                if target > nums[m] and target <= nums[r]:
+                    l = m+1
+                else:
+                    r = m-1
+
+        return -1
 
